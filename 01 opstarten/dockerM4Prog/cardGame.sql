4de975929596439c32c7401fefca578c58e096bf
Ch0ccy-m1lk <louellahanen@outlook.com>
2025-05-13 10:01:51 +0200

    lesson one done

---
+++ 01 opstarten/dockerM4Prog/cardGame.sql
@@ -0,0 +1,6 @@
+CREATE TABLE `pets` (
+  `id` int(11) NOT NULL,
+  `amount games` int(3) NOT NULL,
+  `players` int(2) NOT NULL,
+  `name` varchar(150) NOT NULL,
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
